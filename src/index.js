de523f0f268a5a9d653c9f1098ca177d7b50dbba
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-12-15 16:20:06 -0600

    axios import

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 // Import saga middleware
 import createSagaMiddleware from 'redux-saga';
+import axios from 'axios';
 
 // Create the rootSaga generator function
 function* rootSaga() {
