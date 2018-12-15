e846a9261d0e351952d9860d2c78f58e4c88c29d
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-12-15 17:02:49 -0600

    add connect to redux

--- src/components/ProjectPage/ProjectPage.jsx
+++ src/components/ProjectPage/ProjectPage.jsx
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import ProjectList from '../ProjectList/ProjectList';
 
 class ProjectPage extends Component {
+
+    componentDidMount(){
+        this.getProjects();
+    }
+
+    getProjects(){
+        this.props.dispatch({type: 'FETCH_PROJECTS'})
+    }
+    
     render(){
         return (
             <div>
@@ -10,4 +21,10 @@ class ProjectPage extends Component {
     }
 }
 
-export default ProjectPage;
+const mapStateToProps = (reduxStore) => {
+    return{
+        reduxStore
+    }
+}
+
+export default connect(mapStateToProps)(ProjectPage);
