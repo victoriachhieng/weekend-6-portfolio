18975e36722e51d5f25cd61a5d89becf58b9798c
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-12-15 15:28:18 -0600

    basic setup for projectList add database.sql

--- src/components/ProjectPage/ProjectPage.jsx
+++ src/components/ProjectPage/ProjectPage.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import ProjectList from './ProjectList';
 
 class ProjectPage extends Component {
     render(){
         return (
             <div>
                 <h1>Victoria Chhieng</h1>
+                <ProjectList/>
             </div>
         )
     }
