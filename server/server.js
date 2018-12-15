d943e9971eca1fc4173f745bea4c2bc6de42cc9f
Victoria Chhieng <victoria_chhieng@hotmail.com>
2018-12-15 15:09:21 -0600

    routes added in server.js

--- server/server.js
+++ server/server.js
@@ -3,11 +3,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 
+// Routes
+const projectsRouter = require('./routes/projects.route');
+const tagsRouter = require('./routes/tags.route');
+
+
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
 app.use(express.static('build'));
 
 /** ---------- ROUTES ---------- **/
+app.use('/projects', projectsRouter);
+app.use('/tags', tagsRouter);
 
 
 /** ---------- START SERVER ---------- **/
